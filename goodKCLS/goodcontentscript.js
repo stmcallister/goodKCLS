9e6b10e40f3e5036c35c4b21263979bae637d299
Scott McAllister <stmcallister@gmail.com>
2013-10-15 21:19:03 -0700

    first goodKLCS commit

---
+++ goodKCLS/goodcontentscript.js
@@ -0,0 +1,15 @@
+// get book title from meta tag
+// send back to extension with
+// chrome.extension.sendRequest();
+
+var metas = document.getElementsByTagName('meta');
+
+// scraping title from goodreads meta tags. sending title to library.
+for(var i=0; i < metas.length; i++){
+	var tagProp = metas[i].getAttribute('property');
+	if(tagProp == "og:title"){
+		var title = metas[i].getAttribute('content');
+		chrome.extension.sendRequest({bookTitle: title});
+		break;
+	}
+}
